b9dbe9c00198e9d9fdc818057d44402f515a4b38
pabramsor <pabramsor@gmail.com>
2023-02-04 20:42:41 +0000

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   Belty.py

--- Belty.py
+++ Belty.py
@@ -24,6 +24,30 @@ class BeltControl:
       self.servo.start(0)
 
 
+
+class CameraControl:
+   def __init__(self, pan_pin: int = 33, tilt_pin: int = 32):
+      self.pan_pin = pan_pin
+      self.tilt_pin = tilt_pin
+
+      GPIO.setmode(GPIO.BOARD)
+      GPIO.setup(self.pan_pin, GPIO.OUT)
+      GPIO.setup(self.tilt_pin, GPIO.OUT)
+
+      self.servo_pan = GPIO.PWM(self.pan_pin,500)
+      self.servo_tilt = GPIO.PWM(self.tilt_pin,500)
+
+   def __del__(self):
+      self.stop()
+
+   def move(self, pan: int, tilt: int):
+      self.servo_pan.start(pan)
+      self.servo_tilt.start(tilt)
+
+   def stop(self):
+      self.servo_pan.stop()
+      self.servo_tilt.stop()
+
 if __name__ == "__main__":
    ctl: BeltControl = BeltControl()
 
